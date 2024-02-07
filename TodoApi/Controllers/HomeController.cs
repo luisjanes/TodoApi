94e1c0b57596af170c4140605a2380f270cf4fe7
Luis Felipe da Costa Janes <luisf.janes@gmail.com>
2024-02-07 13:18:14 -0300

    feat: changing the return function to IActionResult

--- TodoApi/Controllers/HomeController.cs
+++ TodoApi/Controllers/HomeController.cs
@@ -9,48 +9,52 @@ namespace TodoApi.Controllers
     public class HomeController : ControllerBase
     {
         [HttpGet("/")]
-        public List<Todo> Get([FromServices] TodoDbContext context)
-        {
-            return context.Todos.ToList();
-        }
+        public IActionResult Get([FromServices] TodoDbContext context) 
+            => Ok(context.Todos.ToList());
+
         [HttpGet("/{id:int}")]
-        public Todo GetbyId(int id, [FromServices] TodoDbContext context)
+        public IActionResult GetbyId(int id, [FromServices] TodoDbContext context)
         {
-            return context.Todos.FirstOrDefault(x => x.Id == id);
+            var Todo = context.Todos.FirstOrDefault(x => x.Id == id);
+            if (Todo == null)
+            {
+                return NotFound("Não encontrado");
+            }
+            return Ok(Todo);
         }
 
         [HttpPost("/todo")]
-        public Todo Post([FromBody] Todo todo, [FromServices]TodoDbContext context)
+        public IActionResult Post([FromBody] Todo todo, [FromServices]TodoDbContext context)
         {
             context.Todos.Add(todo);
             context.SaveChanges();
-            return todo;
+            return Created($"/{todo.Id}",todo);
         }
 
         [HttpPut("/todo/{id:int}")]
-        public Todo Update(int id, [FromBody] Todo todo, [FromServices] TodoDbContext context)
+        public IActionResult Update(int id, [FromBody] Todo todo, [FromServices] TodoDbContext context)
         {
             var model = context.Todos.FirstOrDefault(x=>x.Id == id);
             if (model == null)
-                return todo;
+                return NotFound($"Para o id {id}, não foi encontrado registro");
             model.Title = todo.Title;
             model.Done = todo.Done;
 
             context.Todos.Update(model);
             context.SaveChanges();
-            return model;
+            return Ok(model);
         }
 
         [HttpDelete("/todo/{id:int}")]
-        public Todo Delete(int id, [FromServices] TodoDbContext context)
+        public IActionResult Delete(int id, [FromServices] TodoDbContext context)
         {
             var model = context.Todos.FirstOrDefault(x => x.Id == id);
             if (model == null)
-                return null;
+                return NotFound($"Para o id {id}, não foi encontrado registro");
             model.Id = id;
             context.Todos.Remove(model);
             context.SaveChanges();
-            return model;
+            return Ok(model);
         }
     }
 }
